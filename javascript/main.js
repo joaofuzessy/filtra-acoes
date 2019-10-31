f2bbb8013303fe4a29b2de73e1f43d01604d7d5d
João Füzessy <joao.fuzessy@gmail.com>
2019-10-31 10:45:54 -0300

    Calculo de VA, VPA, QA, PLE e diferença VA-VPA

--- javascript/main.js
+++ javascript/main.js
@@ -38,6 +38,11 @@ return pesquisa;
 function renderPage(resultadoPesquisa){
   var homePageContainer = document.querySelector(".home-page-container");
   var pesquisaContent = document.querySelector(".pesquisa-container .card");
+  var va = resultadoPesquisa[0].dadosacoes.valor;
+  var ple = resultadoPesquisa[0].patrimonio.trimestral;
+  var qa = resultadoPesquisa[0].dadosacoes.quantidade;
+  var vpa = ple/qa;
+  var difVaVpa = va - vpa;
 
   if(homePageContainer){
     homePageContainer.parentNode.removeChild( homePageContainer );
@@ -69,10 +74,18 @@ function renderPage(resultadoPesquisa){
   cardInfo.classList.add('card-info');
   cardInfo.innerHTML = `
                         <ul class="list-group list-group-flush">
-                          <li class="list-group-item"><span class="font-weight-bold">VPA =</span> Valor Patrimonial da Ação (Valor "REAL" da ação)</li>
-                          <li class="list-group-item"><span class="font-weight-bold">PLE =</span> Patrimônio Líquido x Trimestre da Empresa (2019): <span class="font-weight-bold">R$${resultadoPesquisa[0].patrimonio.trimestral}</span></li>
-                          <li class="list-group-item"><span class="font-weight-bold">QA =</span> Quantidade de Ações: <span class="font-weight-bold">${resultadoPesquisa[0].dadosacoes.quantidade}</span></li>
+                          <li class="list-group-item"><span class="c">VPA =</span> Valor Patrimonial da Ação (Valor "REAL" da ação): <span class="font-weight-bold">R$${vpa}</span></li>
+                          <li class="list-group-item"><span class="font-weight-bold">PLE =</span> Patrimônio Líquido x Trimestre da Empresa (2019): <span class="font-weight-bold">R$${ple}</span></li>
+                          <li class="list-group-item"><span class="font-weight-bold">QA =</span> Quantidade de Ações: <span class="font-weight-bold">${qa}</span></li>
                         </ul>
+                        <div>
+                          <p>VA = Valor Atual da ação</p>
+                          <p class="font-weight-bold">R$${va}</p>
+                        </div>
+                        <div>
+                          <p>Diferença de VPA</p>
+                          <p class="font-weight-bold">R$${difVaVpa}</p>
+                        </div>
                         `;
   pesquisaContent.appendChild(cardInfo);
   
